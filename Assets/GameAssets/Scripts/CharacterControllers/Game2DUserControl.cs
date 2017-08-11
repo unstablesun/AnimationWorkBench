da9e9617c794eccd3c8d1cb5521c7fa9df771de9
Dave Hards <davehards@Daves-Mac-mini.local>
2017-08-11 14:38:33 -0700

    animating different characters

---
+++ Assets/GameAssets/Scripts/CharacterControllers/Game2DUserControl.cs
@@ -0,0 +1,38 @@
+using System;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+[RequireComponent(typeof (GameCharacter2D))]
+public class Game2DUserControl : MonoBehaviour
+{
+    private GameCharacter2D m_Character;
+    private bool m_Jump;
+
+
+    private void Awake()
+    {
+		m_Character = GetComponent<GameCharacter2D>();
+    }
+
+
+    private void Update()
+    {
+        if (!m_Jump)
+        {
+            // Read the jump input in Update so button presses aren't missed.
+            m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
+        }
+    }
+
+
+    private void FixedUpdate()
+    {
+        // Read the inputs.
+        bool crouch = Input.GetKey(KeyCode.LeftControl);
+        float h = CrossPlatformInputManager.GetAxis("Horizontal");
+        // Pass all parameters to the character control script.
+        m_Character.Move(h, crouch, m_Jump);
+        m_Jump = false;
+    }
+}
+
