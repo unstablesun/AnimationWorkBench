052bddc2c1f5260b8c96254b7fa1b7d0173d5426
Dave Hards <davehards@Daves-Mac-mini.local>
2017-08-11 12:00:14 -0700

    Tiled plus work bench map

---
+++ RawAssets/Tiled/TileSets/WorkBenchA.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="WorkBenchA" tilewidth="64" tileheight="64" tilecount="3" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="64" height="64" source="BlockGrassPlatform1.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0.666667" y="0.666667" width="62.6667" height="63.3333"/>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <image width="64" height="64" source="BlockRocks1.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0.333333" y="0.666667" width="63.3333" height="63.3333"/>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <image width="64" height="64" source="BlockRocks2.png"/>
+ </tile>
+</tileset>
